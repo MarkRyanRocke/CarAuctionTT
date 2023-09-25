1a5a1671e972c9b356ab425088c2a432ad9565a3
Mark Rocke <markrockecoding@gmail.com>
2023-09-25 13:15:47 -0400

    Auction Service Completed

---
+++ src/AuctionService/Data/Migrations/20230924235300_AddYearToItems.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYearToItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Items",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Items");
+        }
+    }
+}
